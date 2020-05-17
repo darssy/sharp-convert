8e56d697b79706b4950e0ebffeae1c1d18a6dabf
adamstyl <adamastyl@gmail.com>
2020-05-17 22:02:39 +0200

    Attempt to fix author for nuget packages

--- SharpConvert/Properties/AssemblyInfo.cs
+++ SharpConvert/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SharpConvert")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("adamstyl")]
 [assembly: AssemblyProduct("SharpConvert")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
